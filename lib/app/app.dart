05d1caa539de382b297aa4a62f92070a7caa2482
afanzae666 <74753322+afanzae666@users.noreply.github.com>
2026-09-24 00:24:38 +0700

    Implement LifeSimulationApp with simulation engine

---
+++ lib/app/app.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import '../data/repositories/save_repository.dart';
+import '../domain/character/character.dart';
+import '../simulation/engine/simulation_engine.dart';
+import '../simulation/systems/character_system.dart';
+import '../simulation/systems/event_system.dart';
+import '../simulation/systems/time_system.dart';
+import '../core/random/seeded_random.dart';
+import '../presentation/screens/game_screen.dart';
+
+class LifeSimulationApp extends StatelessWidget {
+  const LifeSimulationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final engine = _createEngine();
+
+    return MaterialApp(
+      title: 'Life Simulation',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.indigo,
+        ),
+        useMaterial3: true,
+      ),
+      home: GameScreen(engine: engine),
+    );
+  }
+
+  SimulationEngine _createEngine() {
+    final character = Character.create(
+      id: 'player-1',
+      name: 'Newborn',
+      birthYear: 2026,
+    );
+
+    final engine = SimulationEngine.create(
+      player: character,
+      seed: 20260924,
+      saveRepository: InMemorySaveRepository(),
+    );
+
+    engine.registerSystem(TimeSystem());
+    engine.registerSystem(CharacterSystem());
+    engine.registerSystem(EventSystem());
+
+    return engine;
+  }
+}
