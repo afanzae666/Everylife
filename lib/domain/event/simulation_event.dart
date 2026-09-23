c5638ef7d29a1ac3e770d0985501fc3b1b2a6078
afanzae666 <74753322+afanzae666@users.noreply.github.com>
2026-09-24 00:30:16 +0700

    Add SimulationEvent class and enum for events

---
+++ lib/domain/event/simulation_event.dart
@@ -0,0 +1,22 @@
+enum SimulationEventType {
+  lifeCreated,
+  yearAdvanced,
+  characterAged,
+  randomEvent,
+}
+
+class SimulationEvent {
+  const SimulationEvent({
+    required this.id,
+    required this.type,
+    required this.year,
+    required this.title,
+    required this.description,
+  });
+
+  final String id;
+  final SimulationEventType type;
+  final int year;
+  final String title;
+  final String description;
+}
