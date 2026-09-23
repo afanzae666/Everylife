2e261ad4295eb3752cd7718955d88ae129e53c9c
afanzae666 <74753322+afanzae666@users.noreply.github.com>
2026-09-24 02:18:04 +0700

    Add imports and extend SimulationCommand interface

--- lib/simulation/commands/simulation_command.dart
+++ lib/simulation/commands/simulation_command.dart
@@ -1,3 +1,10 @@
+import '../../core/result/result.dart';
+import '../../domain/world/world_state.dart';
+
 abstract interface class SimulationCommand<T> {
   const SimulationCommand();
+
+  String get id;
+
+  Result<void> validate(WorldState state);
 }
