d07b506ab94c32aa0d5f0033ffed7be963859c58
afanzae666 <74753322+afanzae666@users.noreply.github.com>
2026-09-25 00:46:27 +0700

    Refactor LifeSimulationApp for improved readability

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'app_dependencies.dart';
-import '../data/repositories/save_repository.dart';
 import '../domain/character/character.dart';
 import '../presentation/screens/character_creation_screen.dart';
 import '../presentation/screens/game_screen.dart';
@@ -9,67 +8,65 @@ import '../simulation/engine/simulation_engine.dart';
 import '../simulation/systems/event_system.dart';
 
 class LifeSimulationApp extends StatefulWidget {
-const LifeSimulationApp({
-super.key,
-this.dependencies = const AppDependencies(),
-});
+  const LifeSimulationApp({
+    super.key,
+    this.dependencies = const AppDependencies(),
+  });
 
-final AppDependencies dependencies;
+  final AppDependencies dependencies;
 
-@override
-State<LifeSimulationApp> createState() =>
-_LifeSimulationAppState();
+  @override
+  State<LifeSimulationApp> createState() =>
+      _LifeSimulationAppState();
 }
 
 class _LifeSimulationAppState extends State<LifeSimulationApp> {
-SimulationEngine? _engine;
+  SimulationEngine? _engine;
 
-void _createCharacter(Character character) {
-final engine = _createEngine(character);
+  void _createCharacter(Character character) {
+    final engine = _createEngine(character);
 
-setState(() {
-  _engine = engine;
-});
+    setState(() {
+      _engine = engine;
+    });
+  }
 
-}
+  SimulationEngine _createEngine(
+    Character character,
+  ) {
+    final engine = SimulationEngine.create(
+      player: character,
+      seed: 20260924,
+      saveRepository: widget.dependencies.createSaveRepository(),
+    );
 
-SimulationEngine _createEngine(
-Character character,
-) {
-final engine = SimulationEngine.create(
-player: character,
-seed: 20260924,
-saveRepository: widget.dependencies.createSaveRepository(),
-);
+    engine.registerSystem(
+      EventSystem(
+        random: engine.random,
+      ),
+    );
 
-engine.registerSystem(
-  EventSystem(
-    random: engine.random,
-  ),
-);
+    return engine;
+  }
 
-return engine;
-
-}
-
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-title: 'Life Simulation',
-debugShowCheckedModeBanner: false,
-theme: ThemeData(
-colorScheme: ColorScheme.fromSeed(
-seedColor: Colors.indigo,
-),
-useMaterial3: true,
-),
-home: _engine == null
-? CharacterCreationScreen(
-onCharacterCreated: _createCharacter,
-)
-: GameScreen(
-engine: _engine!,
-),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Life Simulation',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.indigo,
+        ),
+        useMaterial3: true,
+      ),
+      home: _engine == null
+          ? CharacterCreationScreen(
+              onCharacterCreated: _createCharacter,
+            )
+          : GameScreen(
+              engine: _engine!,
+            ),
+    );
+  }
 }
