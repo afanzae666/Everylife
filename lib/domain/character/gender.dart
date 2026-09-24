b3ff0151ac2e573ce08e1edb954bd1cd6d23d4a9
afanzae666 <74753322+afanzae666@users.noreply.github.com>
2026-09-24 21:58:43 +0700

    Add Gender enum with extension for labels

---
+++ lib/domain/character/gender.dart
@@ -0,0 +1,15 @@
+enum Gender {
+  male,
+  female,
+}
+
+extension GenderLabel on Gender {
+  String get label {
+    switch (this) {
+      case Gender.male:
+        return 'Male';
+      case Gender.female:
+        return 'Female';
+    }
+  }
+}
