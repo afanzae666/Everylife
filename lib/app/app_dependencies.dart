4f99310fe606081373190358d98947c5e404995b
afanzae666 <74753322+afanzae666@users.noreply.github.com>
2026-09-27 02:22:49 +0700

    Add SaveManager to AppDependencies class

--- lib/app/app_dependencies.dart
+++ lib/app/app_dependencies.dart
@@ -2,25 +2,42 @@ import '../data/repositories/android_ui_settings_repository.dart';
 import '../data/repositories/json_save_repository.dart';
 import '../data/repositories/save_repository.dart';
 import '../data/repositories/ui_settings_repository.dart';
+import '../data/services/save_manager.dart';
 import '../data/storage/android_json_save_storage.dart';
 
 class AppDependencies {
   const AppDependencies({
     this.saveRepository,
+    this.saveManager,
     this.uiSettingsRepository,
   });
 
   final SaveRepository? saveRepository;
 
+  final SaveManager? saveManager;
+
   final UiSettingsRepository?
       uiSettingsRepository;
 
+  SaveManager createSaveManager() {
+    if (saveManager != null) {
+      return saveManager!;
+    }
+
+    final repository =
+        saveRepository ??
+            JsonSaveRepository(
+              storage:
+                  AndroidJsonSaveStorage(),
+            );
+
+    return SaveManager(
+      repository: repository,
+    );
+  }
+
   SaveRepository createSaveRepository() {
-    return saveRepository ??
-        JsonSaveRepository(
-          storage:
-              AndroidJsonSaveStorage(),
-        );
+    return createSaveManager();
   }
 
   UiSettingsRepository
