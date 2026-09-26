4cd5c2fb6a276a10d8f12f4fefc4e5840386deb3
afanzae666 <74753322+afanzae666@users.noreply.github.com>
2026-09-26 08:44:24 +0700

    Add UiSettingsRepository to AppDependencies

--- lib/app/app_dependencies.dart
+++ lib/app/app_dependencies.dart
@@ -1,18 +1,31 @@
+import '../data/repositories/android_ui_settings_repository.dart';
 import '../data/repositories/json_save_repository.dart';
 import '../data/repositories/save_repository.dart';
+import '../data/repositories/ui_settings_repository.dart';
 import '../data/storage/android_json_save_storage.dart';
 
 class AppDependencies {
   const AppDependencies({
     this.saveRepository,
+    this.uiSettingsRepository,
   });
 
   final SaveRepository? saveRepository;
 
+  final UiSettingsRepository?
+      uiSettingsRepository;
+
   SaveRepository createSaveRepository() {
     return saveRepository ??
         JsonSaveRepository(
-          storage: AndroidJsonSaveStorage(),
+          storage:
+              AndroidJsonSaveStorage(),
         );
+  }
+
+  UiSettingsRepository
+      createUiSettingsRepository() {
+    return uiSettingsRepository ??
+        AndroidUiSettingsRepository();
   }
 }
