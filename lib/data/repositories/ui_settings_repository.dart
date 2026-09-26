2808360cc2868491d2fa5bed6ce909d111d2a7d8
afanzae666 <74753322+afanzae666@users.noreply.github.com>
2026-09-26 08:59:23 +0700

    Refactor UiSettingsRepository interface
    Removed AndroidUiSettingsRepository implementation and its methods.

--- lib/data/repositories/ui_settings_repository.dart
+++ lib/data/repositories/ui_settings_repository.dart
@@ -1,48 +1,5 @@
-import '../storage/android_ui_settings_storage.dart';
-import 'ui_settings_repository.dart';
+abstract interface class UiSettingsRepository {
+  Future<double> loadUiScale();
 
-class AndroidUiSettingsRepository
-    implements UiSettingsRepository {
-  AndroidUiSettingsRepository({
-    AndroidUiSettingsStorage? storage,
-  }) : _storage =
-           storage ??
-           AndroidUiSettingsStorage();
-
-  final AndroidUiSettingsStorage _storage;
-
-  static const double defaultUiScale = 1.0;
-
-  @override
-  Future<double> loadUiScale() async {
-    final value =
-        await _storage.readUiScale();
-
-    if (value == null) {
-      return defaultUiScale;
-    }
-
-    return _sanitize(value);
-  }
-
-  @override
-  Future<void> saveUiScale(
-    double value,
-  ) async {
-    await _storage.writeUiScale(
-      _sanitize(value),
-    );
-  }
-
-  double _sanitize(double value) {
-    if (value < 0.8) {
-      return 0.8;
-    }
-
-    if (value > 1.2) {
-      return 1.2;
-    }
-
-    return value;
-  }
+  Future<void> saveUiScale(double value);
 }
